f100850d194548e9f25e15f2ac4b36ffcce08f1b
gannonsun <2621806555@qq.com>
2021-01-23 21:49:28 +0800

    ts失败，后面再看看

---
+++ src/pages/layout/index.tsx
@@ -0,0 +1,9 @@
+import Header from "@components/Header";
+
+const Layout = (props) => {
+    return (
+        <Header />
+    )
+}
+
+export default Layout;
--- src/router.js
+++ src/router.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Router, BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import Home from './home';
-import Blog from './blog';
+import React from "react";
+import { Router, BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Layout from "./pages/layout/index.tsx";
+import Home from "./home";
+import Blog from "./blog";
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <ul>
-                <li><Link to="/home">home</Link></li>
-                <li><Link to="/blog">blog</Link></li>
-            </ul>
-            <div>
-                {/* Switch只显示一个组件。加exact表示精确匹配。如果不加exact，/xxx也会匹配到 */}
-                <Switch>
-                    <Route exact path="/home" component={Home} />
-                    <Route exact path="/blog" component={Blog} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/layout">layout</Link>
+        </li>
+        <li>
+          <Link to="/home">home</Link>
+        </li>
+        <li>
+          <Link to="/blog">blog</Link>
+        </li>
+      </ul>
+      <div>
+        {/* Switch只显示一个组件。加exact表示精确匹配。如果不加exact，/xxx也会匹配到 */}
+        <Switch>
+          <Route exact path="/layout" component={Layout} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/blog" component={Blog} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default AppRouter;
